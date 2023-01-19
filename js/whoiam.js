e1951dcbf1954568be6ad7f270872a18b7c1b940
Muhammad Andika Putra <mandikaputra7@gmail.com>
2023-01-19 15:04:23 +0700

    Add files via upload

---
+++ js/whoiam.js
@@ -0,0 +1,17 @@
+new Typed('#typed', {
+  strings: [
+    'A.K.A Dikapacman',
+    'Graduate Student',
+    'Video Editor',
+    'Content Creator',
+    'Fresh Graduated',
+    'Infinite Learners',
+    'Creative Thinker',
+    'Educator',
+  ],
+  typeSpeed: 80,
+  backSpeed: 40,
+  backDelay: 500,
+  loop: true,
+  smartBackspace: true,
+});
